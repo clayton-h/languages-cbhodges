45d26c6e930e3c645509daa8efbbaf35e52492c1
ClayT <claytonhodges326@gmail.com>
2023-10-25 09:13:17 -0600

    more_escapes_less_quotations

--- parser/main.py
+++ parser/main.py
@@ -7,47 +7,50 @@ Python library for regular expressions (aka regex)
 
 def string_match(s) -> bool:
     """
-    Regex Breakdown:
-
-    ?: indicates a non-capturing group, it doesn't save captured matched text
-
-    \\. indicates an escaped backslash and dot operator, to match their string literals
-
-    | indicates an alternation operator, matching either the left or right expression
-
     [^"\\] indicates a negated character class, matching any character except a single quote
     or backslash
     """
+
     # Regex pattern to match Python string literals
     pattern = re.compile(r"""
-        (?:                         # Non-capturing group
-            ' (?: \\. | [^'\\] )* '   # Single quoted string
+        (?: # Non-capturing group (doesn't save captured matched text)
+            ' (?: # Single quoted string
+                \\.                 # Escaped backslash and dot operator
+                | [abfnrtv"']       # Match common escape characters
+                | x[0-9A-Fa-f]{2}   # Match hexadecimal escape (\xhh)
+                | [0-7]{1,3}        # Match octal escape (\ooo)
+                | N\{[^}]+\}        # Match Unicode character by name (\N{name})
+                | u[0-9A-Fa-f]{4}   # Match Unicode character by 4-digit hexadecimal
+                | U[0-9A-Fa-f]{8}   # Match Unicode character by 8-digit hexadecimal
+                | r|
+                | [^'\\]            # Don't match a single quote or backslash
+            )* '
         |
-            " (?: \\. | [^"\\] )* "   # Double quoted string
-        |
-            ''' (?: \\. | [^'\\] )* '''  # Triple single quoted string
-        |
-            \"\"\" (?: \\. | [^"\\] )* \"\"\"  # Triple double quoted string
+            " (?: # Double quoted string
+                \\. 
+                | [abfnrtv"']
+                | x[0-9A-Fa-f]{2}
+                | [0-7]{1,3}
+                | N\{[^}]+\}
+                | u[0-9A-Fa-f]{4}
+                | U[0-9A-Fa-f]{8}
+                | r|
+                | [^"\\] 
+            )* " 
         )
     """, re.VERBOSE)
 
-    # Casts and returns a boolean from the fullmatch() function
+    # Casts and returns a True/False from the fullmatch() function
     # (that attempts to match the regex pattern to the entire string)
     return bool(pattern.fullmatch(s))
 
 
 # Test the function with some examples
 examples = [
-    "'hello'",
-    '"world"',
-    "'''hello world'''",
-    '"""hello world"""',
-    "'hello\nworld'",
-    "'hello\\'",
-    "'hello\\nworld'",
-    "'hello world",
-    "hello world'",
-    "hello world"
+    "'hello'",          # Single quoted string
+    '"hello world"',    # Double quoted string
+    "'\a'",             # Bell character
+    r'r'\n'             # Raw string with newline
 ]
 
 
