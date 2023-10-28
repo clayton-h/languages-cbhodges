733bdccb884d832d8a61178bcbf394baf3da5829
ClayT <claytonhodges326@gmail.com>
2023-10-28 15:28:33 -0600

    working_iteration

--- parser/main.py
+++ parser/main.py
@@ -13,8 +13,8 @@ def string_match(s) -> bool:
 
     # Regex pattern to match Python string literals
     pattern = re.compile(r"""
-        (?:
-            (?:                     # Non-capturing group (doesn't save captured matched text)
+        (?:                         # Non-capturing group (doesn't save captured matched text)
+            '(?:                    # Single-quoted strings
                 \\.                 # Escaped backslash and dot operator
                 | [abfnrtv"']       # Match common escape characters
                 | x[0-9A-Fa-f]{2}   # Match hexadecimal escape (\xhh)
@@ -24,7 +24,19 @@ def string_match(s) -> bool:
                 | U[0-9A-Fa-f]{8}   # Match Unicode character by 8-digit hexadecimal
                 | r|                # Match a raw string
                 | [^'\\]            # Don't match a single quote or backslash
-            )*
+            )*'
+            |
+            "(?:                    # Double-quoted strings
+                \\.                 # Escaped backslash and dot operator
+                | [abfnrtv"']       # Match common escape characters
+                | x[0-9A-Fa-f]{2}   # Match hexadecimal escape (\xhh)
+                | [0-7]{1,3}        # Match octal escape (\ooo)
+                | N\{[^}]+\}        # Match Unicode character by name (\N{name})
+                | u[0-9A-Fa-f]{4}   # Match Unicode character by 4-digit hexadecimal
+                | U[0-9A-Fa-f]{8}   # Match Unicode character by 8-digit hexadecimal
+                | r|                # Match a raw string
+                | [^'\\]            # Don't match a single quote or backslash
+            )*"
         )
     """, re.VERBOSE)
 
@@ -35,8 +47,12 @@ def string_match(s) -> bool:
 
 # Test the function with some examples
 examples = [
-    "hello",
-    'world'
+    '"hello"',  # double quoted string - exp: True
+    "'world'",  # single quoted string - exp: True
+    "'\a'",     # bell character - exp: True
+    "'\\'",     # escaped backslash - exp: False
+    "'\''",     # escaped single quote - exp: False
+    "''"
 ]
 
 
